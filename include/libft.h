265a7ce4cc1339a257788eb47c8fac90f8723b0f
Mahmut Kilic <71298914+hetzwaard@users.noreply.github.com>
2025-01-02 10:09:41 +0100

    Add files via upload

--- include/libft.h
+++ include/libft.h
@@ -22,7 +22,7 @@
 # include <string.h>
 # include <unistd.h>
 
-int	ft_atoi(const char *nptr);
+int		ft_atoi(const char *nptr);
 
 long	ft_atol(const char *str);
 
@@ -30,19 +30,19 @@ void	ft_bzero(void *s, size_t n);
 
 void	*ft_calloc(size_t nmemb, size_t size);
 
-int	ft_isalpha(int c);
-int	ft_isdigit(int c);
-int	ft_isalnum(int c);
-int	ft_isascii(int c);
-int	ft_isspace(int c);
-int	ft_isprint(int c);
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isalnum(int c);
+int		ft_isascii(int c);
+int		ft_isspace(int c);
+int		ft_isprint(int c);
 
 void	*ft_memset(void *s, int c, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t len);
 void	*ft_memchr(const void *s, int c, size_t n);
 
-int	ft_memcmp(const void *s1, const void *s2, size_t n);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
 
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strchr(const char *s, int c);
@@ -54,7 +54,7 @@ char	*ft_strtrim(char const *s1, char const *set);
 
 char	*ft_itoa(int n);
 
-int	ft_strncmp(const char *s1, const char *s2, size_t n);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
 
 void	ft_striteri(char *s, void (*f)(unsigned int, char *));
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
@@ -71,7 +71,7 @@ void	ft_putstr_fd(char *str, int fd);
 void	ft_putendl_fd(char *str, int fd);
 void	ft_putnbr_fd(int nb, int fd);
 
-int	ft_toupper(int c);
-int	ft_tolower(int c);
+int		ft_toupper(int c);
+int		ft_tolower(int c);
 
 #endif
